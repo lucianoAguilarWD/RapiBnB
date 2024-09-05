326e0b00ebeba93fffd3da19f5cff4e4ca5b76ff
Luciano Aguilar <166865911+lucianoAguilarWD@users.noreply.github.com>
2024-09-04 22:08:23 -0300

    arreglo en el router

--- App/router.php
+++ App/router.php
@@ -15,7 +15,7 @@ class Router
 
         $url = explode('/', URL);
 
-        $this->controller = !empty($url[1]) ? $url[1] : 'page';
+        $this->controller = !empty($url[1]) ? $url[1] : 'Page';
         $this->method = !empty($url[2]) ? $url[2] : 'home';
 
         $this->controller = $this->controller . 'Controller';
